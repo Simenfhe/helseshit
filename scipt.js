1c6dce83ca58c4bff06fc1918ee2b5895ed61fb1
BuildTools <simenfhe@stud.ntnu.no>
2022-12-13 23:11:21 +0100

    update

--- scipt.js
+++ scipt.js
@@ -13,9 +13,12 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  console.log(slides.length)
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
@@ -30,3 +33,17 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+var amt = document.querySelectorAll('.column');
+console.log(amt[1])
+var size = 100 / amt.length;
+console.log(size);
+var text = size.toString();
+console.log(text);
+var percent = text.concat("%");
+console.log(percent);
+
+amt.forEach(amt =>{
+  amt.style.width = percent;
+});
